cfc46ccfed182437c686eaf7cb18db7bac390a85
Flavio Bravo <f980534860@gmail.com>
2019-11-21 16:48:06 -0500

    handle the api request with the cache

--- server/routes.js
+++ server/routes.js
@@ -3,14 +3,37 @@ var express = require('express');
 var router = express.Router();
 
 
+const mensajes = [
 
+  {
+    _id: 'XXX',
+    user: 'spiderman',
+    mensaje: 'Hola mundo'
+  },
+  {
+    _id: 'XXX',
+    user: 'Ironman',
+    mensaje: 'Hola mundo'
+  },
+  {
+    _id: 'XXX',
+    user: 'Hulk',
+    mensaje: 'Hola mundo'
+  },
+  {
+    _id: 'XXX',
+    user: 'Thor',
+    mensaje: 'Hola mundo'
+  }
+];
 
 
 
 
 // Get mensajes
 router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+  //res.json('Obteniendo mensajes');
+  res.json( mensajes );
 });
 
 
